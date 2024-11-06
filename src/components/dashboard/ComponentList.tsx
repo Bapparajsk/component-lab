a7f5c838edf0f85aa0ca21e8b396d84021de27eb
Bapparaj sk <bapparajsk97@gmail.com>
2024-11-06 19:14:11 +0530

    feat(dashboard): Implement PostCard Component with Progress Indicators
    Adds a new PostCard component that displays the progress of a component through different stages. The component includes visual indicators for each stage, such as a progress bar and timestamps for the upload and verification dates.

--- src/components/dashboard/ComponentList.tsx
+++ src/components/dashboard/ComponentList.tsx
@@ -1,14 +1,192 @@
 "use client";
 
+import { useState } from "react";
 import { Button, Input, Pagination } from "@nextui-org/react";
 import { IconBookUpload } from "@tabler/icons-react";
 import { useRouter  } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
 
 import { PostCard, QueryButton } from "@/components/dashboard";
+import axios from "@/lib/axios";
+
+
+type PostCard = {
+  id: string;
+  title: string;
+  description: string;
+  uploadDate?: Date;
+  verifyDate?: Date;
+  progress?: string;
+  likes?: Map<string, string>;
+  codePreview?: Map<string, string>;
+  tags?: string[];
+  createdAt?: Date;
+  timeProgress?: {
+    step1: string,
+    date: Date | string,
+  }[];
+};
+
+const TempPostCard: PostCard[] = [
+  {
+    id: "1",
+    title: "Button",
+    description: "A simple button component",
+    uploadDate: new Date(),
+    verifyDate: new Date(),
+    progress: "approved",
+    timeProgress: [
+      {
+        step1: "Uploading",
+        date: new Date(),
+      },
+      {
+        step1: "approved",
+        date: new Date(),
+      },
+      {
+        step1: "creating-files",
+        date: "progress",
+      },
+      {
+        step1: "completed",
+        date: "-",
+      }
+    ]
+  },
+  {
+    id: "2",
+    title: "Input",
+    description: "A simple input component",
+    uploadDate: new Date(),
+    verifyDate: new Date(),
+    progress: "creating-files",
+    timeProgress: [
+      {
+        step1: "Uploading",
+        date: new Date(),
+      },
+      {
+        step1: "approved",
+        date: new Date(),
+      },
+      {
+        step1: "creating-files",
+        date: new Date(),
+      },
+      {
+        step1: "completed",
+        date: "progress",
+      }
+    ]
+  },
+  {
+    id: "3",
+    title: "Card",
+    description: "A simple card component",
+    uploadDate: new Date(),
+    verifyDate: new Date(),
+    progress: "pending",
+    timeProgress: [
+      {
+        step1: "Uploading",
+        date: new Date(),
+      },
+      {
+        step1: "approved",
+        date: "progress",
+      },
+      {
+        step1: "creating-files",
+        date: "-",
+      },
+      {
+        step1: "completed",
+        date: "-",
+      }
+    ]
+  },
+  {
+    id: "4",
+    title: "Modal",
+    description: "A simple modal component",
+    uploadDate: new Date(),
+    verifyDate: new Date(),
+    progress: "rejected",
+  },
+  {
+    id: "5",
+    title: "Alert",
+    description: "A simple alert component",
+    likes: new Map<string, string>(),
+    codePreview: new Map<string, string>(),
+    tags: ["alert", "component"],
+    createdAt: new Date(),
+  },
+  {
+    id: "6",
+    title: "Select",
+    description: "A simple select component",
+    likes: new Map<string, string>(),
+    codePreview: new Map<string, string>(),
+    tags: ["select", "component"],
+    createdAt: new Date(),
+  },
+  {
+    id: "7",
+    title: "Checkbox",
+    description: "A simple checkbox component",
+    uploadDate: new Date(),
+    verifyDate: new Date(),
+    progress: "approved",
+    timeProgress: [
+      {
+        step1: "Uploading",
+        date: new Date(),
+      },
+      {
+        step1: "approved",
+        date: new Date(),
+      },
+      {
+        step1: "creating-files",
+        date: "progress",
+      },
+      {
+        step1: "completed",
+        date: "-",
+      }
+    ]
+  }
+];
 
 export const ComponentList = () => {
+  const [pageNumber, setPageNumber] = useState(1);
 
   const router = useRouter();
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["components", pageNumber],
+    queryFn: ({  }) => {
+      // const [, page] = queryKey as [string, number];
+      // const response = await axios.get(`/post/list?env=all&page=${page-1}`);
+      // return response.data.data;
+      TempPostCard.map((post) => {
+        if (!post.progress) {
+          post.likes = new Map<string, string>();
+          post.codePreview = new Map<string, string>();
+
+          post.likes.set("1", "1");
+          post.likes.set("2", "2");
+          post.likes.set("3", "3");
+          post.likes.set("4", "4");
+          post.codePreview.set("5", "5");
+          post.codePreview.set("6", "6");
+        }
+      });
+      return {posts: TempPostCard};
+    },
+  });
 
   return (
     <div className={"w-full h-auto py-4 px-3 flex gap-x-2 flex-col items-center"}>
@@ -32,21 +210,26 @@ export const ComponentList = () => {
             </Button>
           </div>
         </div>
-        <PostCard
-          title={"Upload Option"}
-          id={"upload-option"}
-          description={"This is a simple upload option for your project. This is a simple upload option for your project. This is a simple upload option for your project. This is a simple upload option for your project. This is a simple upload option for your project."}
-        />
+        {isLoading && (
+          <div className={"w-full h-auto flex items-center justify-center"}>
+            <p>Loading...</p>
+          </div>
+        )}
+        {data?.posts.map((post: any) => (
+            <PostCard
+              key={post.id}
+              {...post}
+            />
+        ))}
         <div className={"w-full h-auto flex justify-end"}>
           <Pagination
             showShadow={true}
             isCompact={true}
             showControls={true}
-            total={20}
+            // total={data?.totalPages || 1}
+            total={1}
             initialPage={1}
-            onChange={(page: number) => {
-              console.log(page);
-            }}
+            onChange={(page: number) => { setPageNumber(page); }}
           />
         </div>
       </div>
--- src/components/dashboard/PostCard.tsx
+++ src/components/dashboard/PostCard.tsx
@@ -4,65 +4,109 @@ import { Button } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 
 import { NumberTicker } from "@/components/ui/number-ticker";
+import { cn } from "@/lib/utils";
 
 type CardProps = {
-  title: string;
-  id: string;
-  description?: string;
-  tag?: {id: string, value: string}[];
-  likes?: number;
-  codePreview?: number;
+  id: string | null,
+  title: string,
+  description?: string,
+  progress?: "pending" | "approved" | "creating-files" | "completed" | "rejected";
+  likes: Map<string, string>,
+  codePreview: Map<string, string>,
+  tags: string[],
+  timeProgress: {
+    step1: string,
+    date: Date | "progress" | "-",
+  }[],
 };
 
+const getColorByProgress = (progress: string | undefined) => {
+
+
+  switch (progress) {
+    case "pending":
+      return "#7CF5FF";
+    case "approved":
+      return "#9BEC00";
+    case "creating-files":
+      return "#006BFF";
+    case "rejected":
+      return "#D91656";
+    default:
+      return "";
+  }
+};
 
 export const PostCard = ({
-  title,
   id,
+  title,
   description,
-  tag,
+  timeProgress,
+  progress,
   likes,
   codePreview,
+  tags
 }: CardProps) => {
-
   const router = useRouter();
 
+  const d = new Date();
+  d.toDateString();
+
   return (
-    <div className={"w-full h-auto min-h-28 flex items-center justify-center "}>
-      <div className={"w-full flex border-b border-gray-600"}>
-        <div className={"w-1/2 h-auto flex flex-col items-start justify-between gap-y-3 px-3 py-4 font-rubik"}>
-          <div>
+    <div className={cn("w-full h-auto min-h-28 flex items-center justify-center  rounded-md ")}>
+      <div
+        className={cn("w-full flex border-b", `border-[${getColorByProgress(progress)}] shadow-[inset_0px_-50px_50px_-50px_${getColorByProgress(progress)}]`)}
+        style={{ borderColor: getColorByProgress(progress), backgroundColor: getColorByProgress(progress) + "12" }}
+      >
+        <div className={`h-auto flex flex-col items-start justify-between gap-y-3 px-3 py-4 font-rubik`}
+             style={{ width: progress ? "100%" : "50%" }}>
+          <div className={"w-full flex items-center justify-between"}>
             <h1
               onClick={() => router.push(`/components/button/${id}`)} // Path: src/components/button/[id].tsx
-              className={"w-fit text-3xl text-blue-500 tracking-wider cursor-pointer transition-all duration-300 hover:underline"}
-            >{title}</h1>
+              className={cn("w-fit text-3xl text-blue-500 tracking-wider cursor-pointer transition-all duration-300 hover:underline", progress === "rejected" && "text-red-500 hover:no-underline")}
+            >{title} <span className={"text-sm"}> {progress === "rejected" && "Your component hash ben rejected"}</span>
+            </h1>
+            {progress && <Button onClick={() => router.push(`/components/button/${id}`)} size={"sm"} variant={"flat"}
+                                 color={"danger"}>{progress === "rejected" ? "Delete" : "Edit"}</Button>}
           </div>
           {description && (
             <div>
-              <p
-                className={"text-medium tracking-wide"}
-              >
+              <p className={"text-medium tracking-wide"}>
                 {description.length > 200 ? description.slice(0, 200) + "..." : description}
               </p>
             </div>
           )}
           <div className={"flex flex-wrap gap-4 items-center justify-start"}>
-            {tag && tag.map((t) => (
-              <Button key={t.id}>{t.value}</Button>
+            {tags && tags.map((t, idx) => (
+              <Button key={idx}>{t}</Button>
             ))}
           </div>
+          {progress && progress !== "rejected" && <div className={"w-full py-3"}>
+            <div className={"w-full h-auto flex flex-col justify-between items-start text-sm relative gap-5"}>
+              {timeProgress && timeProgress.map((item, index) => (
+                <div key={index} className={"flex items-start justify-start gap-2 relative px-5"}>
+                  <p>{item.step1}</p>
+                  <p>{
+                    item.date === "progress" ? "progress..." : item.date == "-" ?  " -" : new Date(item.date).toDateString()
+                  }</p>
+                </div>
+              ))}
+              <div className={"absolute top-0 left-0 h-full w-[2px] -z-10 rounded-md"} style={{ backgroundColor: getColorByProgress(progress) }}/>
+            </div>
+          </div>}
         </div>
-        <div className={"w-1/2 h-full my-auto"}>
+        {codePreview && <div className={"w-1/2 h-full my-auto"}>
           <div className={"w-full h-full flex flex-col items-center justify-center gap-y-3 text-2xl font-bold"}>
             <div className={"flex items-center gap-5"}>
               <p>Like</p>
-              {likes ? (<NumberTicker value={likes} />) : (<p>0</p>)}
+              {likes ? (<NumberTicker value={likes.size} />) : (<p className={"text-red-500"}>-</p>)}
             </div>
             <div className={"flex items-center gap-5 "}>
               <p>Code Preview</p>
-              {codePreview ? (<NumberTicker value={codePreview} />) : (<p>0</p>)}
+              {codePreview ? (<NumberTicker value={codePreview.size} />) : (<p>0</p>)}
             </div>
           </div>
-        </div>
+        </div>}
       </div>
     </div>
   );
